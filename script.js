e59fa69b26b96d8978bddf90f5f0ed042c297dc6
Nathan Baird <nate19981@googlemail.com>
2022-09-02 16:51:00 +0100

    Player movement selection added

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function playGame() {
     let computerMove = getComputerChoice();
+    let playerMove = playerSelection();
 
     console.log(computerMove);
+    console.log(`This is the players move: ${playerMove}`);
+}
+
+function playerSelection() {
+    let playerChoice = prompt('Enter "Rock", "Paper", or "Scissors" to play.');
+    if (playerChoice != null) {
+        playerChoice = formatPlayerInput(playerChoice);
+        return playerChoice;
+    }
 }
 
 //Selects either Rock, Paper, or Scissors for the computers move.
@@ -18,4 +28,14 @@ function getComputerChoice() {
         default:
             alert('Something went wrong');
     }
+}
+
+function formatPlayerInput(playerChoice) {
+    let firstChar = playerChoice.charAt(0);
+    firstChar = firstChar.toUpperCase();
+    let remainingChars = playerChoice.substring(1);
+    remainingChars = remainingChars.toLowerCase();
+
+    let formattedString = `${firstChar}${remainingChars}`;
+    return formattedString;
 }
