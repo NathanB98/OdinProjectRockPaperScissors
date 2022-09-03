4d827957d621089b19128448d31d0599bbf4dabc
Nathan Baird <nate19981@googlemail.com>
2022-09-03 18:21:32 +0100

    Playing a single round is now possible. Reworked JS for better approach

--- script.js
+++ script.js
@@ -2,15 +2,49 @@ function playGame() {
     let computerMove = getComputerChoice();
     let playerMove = playerSelection();
 
-    console.log(computerMove);
-    console.log(`This is the players move: ${playerMove}`);
+    playRound(computerMove, playerMove);
 }
 
+//Runs a single round of Rock, Paper, Scissors. Uses computerMove and playerMove arguments from earlier functions.
+function playRound(computerMove, playerMove) {
+    if(playerMove == 'Rock') {
+        if(computerMove == 'Scissors') {
+            console.log('You win! Rock beats Scissors');
+        } else if(computerMove == 'Paper') {
+            console.log('You lose! Paper beats Rock');
+        } else {
+            console.log('The round is a draw!');
+        }
+    } else if(playerMove == 'Paper') {
+        if(computerMove == 'Rock') {
+            console.log('You win! Paper beats Rock');
+        } else if (computerMove == 'Scissors') {
+            console.log('You lose! Scissors beats Paper');
+        } else {
+            console.log('The round is a draw!');
+        }
+    } else {
+        if(computerMove == 'Paper') {
+            console.log('You win! Scissors beats Paper');
+        } else if (computerMove == 'Rock') {
+            console.log('You lose! Rock beats Scissors');
+        } else {
+            console.log('The round is a draw!');
+        }
+    }
+}
+
+//Take input from user. Check if it is suitable. Return the move.
 function playerSelection() {
     let playerChoice = prompt('Enter "Rock", "Paper", or "Scissors" to play.');
     if (playerChoice != null) {
         playerChoice = formatPlayerInput(playerChoice);
+    }
+
+    if(playerChoice == 'Rock' || playerChoice == 'Paper' || playerChoice == 'Scissors'){
         return playerChoice;
+    } else {
+        alert('Please enter a valid move');
     }
 }
 
@@ -25,8 +59,6 @@ function getComputerChoice() {
             return 'Paper';
         case 2:
             return 'Scissors';
-        default:
-            alert('Something went wrong');
     }
 }
 
