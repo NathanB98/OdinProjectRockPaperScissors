bf0e842d8ee2b0c59a32b4e67aa77b5b697f6291
Nathan Baird <nate19981@googlemail.com>
2022-09-04 20:44:46 +0100

    Each button working as intended. All outcomes display, div created in JS

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const buttons = document.querySelectorAll('button');
 
+let scoreContainer = document.createElement('div');
+let scorePara = document.createElement('p');
+
 let playerChoice;
 
 //Eventlistener waits for user to pick a button, representing a move. Players choice is stored then the game function is called.
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        
         if(button.id == 'rock') {
             playerChoice = 'Rock';
         } else if (button.id == 'paper') {
@@ -25,35 +27,58 @@ function playGame(playerChoice) {
     playRound(computerMove, playerChoice);
 }
 
-//Runs a single round of Rock, Paper, Scissors. Uses computerMove and playerMove arguments from earlier functions.
+//Runs a single round of Rock, Paper, Scissors.
 function playRound(computerMove, playerMove) {
+    let result;
+
     if(playerMove == 'Rock') {
         if(computerMove == 'Scissors') {
-            console.log('You win! Rock beats Scissors');
+            result = 'win';
+            updateRoundResult(result, computerMove, playerMove);
         } else if(computerMove == 'Paper') {
-            console.log('You lose! Paper beats Rock');
+            result = 'lose';
+            updateRoundResult(result, computerMove, playerMove);
         } else {
-            console.log('The round is a draw!');
+            result = 'draw';
+            updateRoundResult(result, computerMove, playerMove);
         }
     } else if(playerMove == 'Paper') {
         if(computerMove == 'Rock') {
-            console.log('You win! Paper beats Rock');
+            result = 'win';
+            updateRoundResult(result, computerMove, playerMove);
         } else if (computerMove == 'Scissors') {
-            console.log('You lose! Scissors beats Paper');
+            result = 'lose';
+            updateRoundResult(result, computerMove, playerMove);
         } else {
-            console.log('The round is a draw!');
+            result = 'draw';
+            updateRoundResult(result, computerMove, playerMove);
         }
     } else {
         if(computerMove == 'Paper') {
-            console.log('You win! Scissors beats Paper');
+            result = 'win';
+            updateRoundResult(result, computerMove, playerMove);
         } else if (computerMove == 'Rock') {
-            console.log('You lose! Rock beats Scissors');
+            result = 'lose';
+            updateRoundResult(result, computerMove, playerMove);
         } else {
-            console.log('The round is a draw!');
+            result = 'draw';
+            updateRoundResult(result, computerMove, playerMove);
         }
     }
 }
 
+//Updates the div that displays the last rounds results.
+function updateRoundResult(result, computerMove, playerMove) {
+    scoreContainer = document.querySelector('#gameContainer');
+    if(result == 'win') {
+        scorePara.textContent = `You ${result}! ${playerMove} beats ${computerMove}`;
+    } else if(result == 'lose') {
+        scorePara.textContent = `You ${result}! ${computerMove} beats ${playerMove}`;
+    } else {
+        scorePara.textContent = `The round is a ${result}!`;
+    }
+    scoreContainer.appendChild(scorePara);
+}
 
 //Selects either Rock, Paper, or Scissors for the computers move.
 function getComputerChoice() {
